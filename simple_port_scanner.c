e4b1c5d8dbdd5cda31ad1423fe214c207dc2b169
5past1 <136489734+5past1@users.noreply.github.com>
2023-06-18 22:02:54 -0700

    Add files via upload
    frame for the program

---
+++ simple_port_scanner.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+void scan_port(const char host[], int port) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        perror("socket");
+        return;
+    }
+
+    struct sockaddr_in server = {0};
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = inet_addr(host);
+    server.sin_port = htons(port);
+
+    if (connect(sock, (struct sockaddr*)&server, sizeof(server)) < 0) {
+        printf("Port %d is closed\n", port);
+    } else {
+        printf("Port %d is open\n", port);
+    }
+
+    close(sock);
+}
+
+int main() {
+    char host[100];
+    int choice, port;
+
+    while (1) {
+        printf("1. Scan all ports\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter the IP address to scan: ");
+                scanf("%s", host);
+
+                for (port = 1; port <= 65535; port++) {
+                    scan_port(host, port);
+                }
+
+                break;
+            case 5:
+                printf("Exiting...\n");
+                exit(0);
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    }
+
+    return 0;
+}
+
